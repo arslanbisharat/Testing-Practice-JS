262bcc27c2aa855fde5a34b0f04af899da0ca609
arslanbisharat <thearslan380@gmail.com>
2020-07-09 01:24:12 -0700

    linter

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,10 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable indent */
+/* eslint-disable no-trailing-spaces */
+// eslint-disable-next-line linebreak-style
+/* eslint-disable semi */
+
 function capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1) 
+  return string[0].toUpperCase() + string.slice(1);
 }
 module.exports = capitalize;
